93cae07dd9181d5c5b4733d6c49224e963e61408
Maxim Podgorov <mapodgorov@gmail.com>
2020-05-27 21:04:12 +0700

    Restart on collision with enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{    
+    private Transform _playerTransform;
+
     private int _gemCount;
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _playerTransform = GetComponent<Transform>();
+
+        _startPosition = _playerTransform.position;        
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,6 +23,16 @@ public class Player : MonoBehaviour
             _gemCount++;
             Debug.Log("Total gem count: " + _gemCount);
             Destroy(collision.gameObject);
+        }        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out PatrolMovement enemy))
+        {
+            Debug.Log("Придется начинать сначала.");
+            _playerTransform.position = _startPosition;
+
         }
-    }    
+    }
 }
