0040aac86ca4655b022a769f3c9d3ae972c03bde
Maxim Podgorov <mapodgorov@gmail.com>
2020-05-29 11:06:49 +0700

    name fix

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,7 +6,7 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _template;
     [SerializeField] private float _respawnDelay = 0f;
-    [SerializeField] private float _respawnLoopTime = 2f;
+    [SerializeField] private float _respawnLoopTimer = 2f;
 
     private float _respawnTime = 0f;
 
@@ -14,7 +14,7 @@ public class Spawner : MonoBehaviour
     {
         _respawnTime += Time.deltaTime;
 
-        if (_respawnTime >= _respawnLoopTime + _respawnDelay)
+        if (_respawnTime >= _respawnLoopTimer + _respawnDelay)
         {
             GameObject newTemplate = Instantiate(_template, transform.position, Quaternion.identity);
             _respawnTime = _respawnDelay;
