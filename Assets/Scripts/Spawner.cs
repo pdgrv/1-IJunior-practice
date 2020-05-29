c9f101fa6b22432a277d3ac5f858a93f98169659
Maxim Podgorov <mapodgorov@gmail.com>
2020-05-29 10:51:46 +0700

    Fix spawner class style and naming

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private float _respawnDelay = 0f;
+    [SerializeField] private float _respawnLoopTime = 2f;
+
+    private float _respawnTime = 0f;
+
+    private void Update()
+    {
+        _respawnTime += Time.deltaTime;
+
+        if (_respawnTime >= _respawnLoopTime + _respawnDelay)
+        {
+            GameObject newTemplate = Instantiate(_template, transform.position, Quaternion.identity);
+            _respawnTime = _respawnDelay;
+        }
+    }
+}
